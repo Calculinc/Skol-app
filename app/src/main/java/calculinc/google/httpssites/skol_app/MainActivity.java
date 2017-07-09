dc0f47b30373e9f49df34b4311891edb2c86e8bd
Dodden <david.bardvall@gmail.com>
2017-07-09 21:37:46 +0200

    Added login-save function

--- app/src/main/java/calculinc/google/httpssites/skol_app/MainActivity.java
+++ app/src/main/java/calculinc/google/httpssites/skol_app/MainActivity.java
@@ -149,8 +149,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private Menu menu;
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
