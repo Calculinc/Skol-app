fd6c06404b071bbd25d8cc3f55a3b06b4a60a318
ruboss <Rubosssonofrub@gmail.com>
2017-11-25 00:35:10 +0100

    fixed the "cigg"

--- app/src/main/java/calculinc/google/httpssites/skol_app/MainActivity.java
+++ app/src/main/java/calculinc/google/httpssites/skol_app/MainActivity.java
@@ -703,25 +703,13 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -1329,6 +1317,7 @@ public class MainActivity extends AppCompatActivity
         final Button button = (Button) findViewById(R.id.matsedel_button);
 
         button.setClickable(false);
+        //lol
 
         view.startAnimation(anim_button_click);
         final String matrating = String.valueOf(ratingBar.getRating()).replace(".",",");
